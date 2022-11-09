50ff5e0ae81120c1bbbc47a14ec29655a2c6722d
Jerry Martins <dtd_jersouza@uolinc.com>
2022-11-09 14:54:24 -0300

    finish poc

--- src/main/java/br/com/pagseguro/servicelayer/controller/response/BillsResponse.java
+++ src/main/java/br/com/pagseguro/servicelayer/controller/response/BillsResponse.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -12,10 +14,98 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BillsResponse {
-    private List<LinkResponse> links;
-    private PaginationResponse pagination;
-    private boolean firstTimeUser;
-    private boolean boletoEnabled;
-    private boolean boletoBlocked;
-    private LimitResponse boletoLimit;
+    public List<LinkResponse> links;
+    public PaginationResponse pagination;
+    public boolean firstTimeUser;
+    public boolean boletoEnabled;
+    public boolean boletoBlocked;
+    public LimitResponse boletoLimit;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LinkResponse {
+        public String url;
+        public String linkId;
+        public String linkName;
+        public String merchant;
+        public Long amount;
+        public LocalDateTime creation;
+        public LocalDateTime updated;
+        public LocalDate dueDate;
+        public int daysToExpire;
+        public String status;
+        public DebtorResponse debtor;
+        public String paymentCode;
+        public String chargeId;
+        public String userAgent;
+        public String paymentStatus;
+
+        @Data
+        @Builder
+        @AllArgsConstructor
+        @NoArgsConstructor
+        public static class DebtorResponse {
+            public String name;
+            public DebtorDocumentResponse debtorDocument;
+            public String email;
+            public DebtorPhoneResponse debtorPhone;
+            public DebtorAddressResponse debtorAddress;
+            public boolean registered;
+
+            @Data
+            @Builder
+            @AllArgsConstructor
+            @NoArgsConstructor
+            public static class DebtorDocumentResponse {
+                public String type;
+                public String number;
+            }
+
+            @Data
+            @Builder
+            @AllArgsConstructor
+            @NoArgsConstructor
+            public static class DebtorPhoneResponse {
+                public String areaCode;
+                public String number;
+            }
+
+            @Data
+            @Builder
+            @AllArgsConstructor
+            @NoArgsConstructor
+            public static class DebtorAddressResponse  {
+                public String street;
+                public String number;
+                public String complement;
+                public String district;
+                public String postalCode;
+                public String city;
+                public String state;
+                public boolean defaultAddress;
+            }
+        }
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class PaginationResponse {
+        public int total;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LimitResponse {
+        public Long totalValue;
+        public Long currentValue;
+        public Integer totalQuantity;
+        public Integer currentQuantity;
+    }
+
 }
--- src/main/java/br/com/pagseguro/servicelayer/model/BillsModel.java
+++ src/main/java/br/com/pagseguro/servicelayer/model/BillsModel.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -12,10 +14,98 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BillsModel {
-    private List<LinkModel> links;
-    private PaginationModel pagination;
-    private boolean firstTimeUser;
-    private boolean boletoEnabled;
-    private boolean boletoBlocked;
-    private LimitModel boletoLimit;
+    public List<LinkModel> links;
+    public PaginationModel pagination;
+    public boolean firstTimeUser;
+    public boolean boletoEnabled;
+    public boolean boletoBlocked;
+    public LimitModel boletoLimit;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LinkModel {
+        public String url;
+        public String linkId;
+        public String linkName;
+        public String merchant;
+        public Long amount;
+        public LocalDateTime creation;
+        public LocalDateTime updated;
+        public LocalDate dueDate;
+        public int daysToExpire;
+        public String status;
+        public DebtorModel debtor;
+        public String paymentCode;
+        public String chargeId;
+        public String userAgent;
+        public String paymentStatus;
+
+        @Data
+        @Builder
+        @AllArgsConstructor
+        @NoArgsConstructor
+        public static class DebtorModel {
+            public String name;
+            public DebtorDocumentModel debtorDocument;
+            public String email;
+            public DebtorPhoneModel debtorPhone;
+            public DebtorAddressModel debtorAddress;
+            public boolean registered;
+
+            @Data
+            @Builder
+            @AllArgsConstructor
+            @NoArgsConstructor
+            public static class DebtorDocumentModel {
+                public String type;
+                public String number;
+            }
+
+            @Data
+            @Builder
+            @AllArgsConstructor
+            @NoArgsConstructor
+            public static class DebtorPhoneModel {
+                public String areaCode;
+                public String number;
+            }
+
+            @Data
+            @Builder
+            @AllArgsConstructor
+            @NoArgsConstructor
+            public static class DebtorAddressModel  {
+                public String street;
+                public String number;
+                public String complement;
+                public String district;
+                public String postalCode;
+                public String city;
+                public String state;
+                public boolean defaultAddress;
+            }
+        }
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class PaginationModel {
+        public int total;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LimitModel {
+        public Long totalValue;
+        public Long currentValue;
+        public Integer totalQuantity;
+        public Integer currentQuantity;
+    }
+
 }
